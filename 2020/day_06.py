c95bc93917daa1b8ef15d4b96906811ea9ade8d7
kirienko <yury.kirienko@gmail.com>
2020-12-06 21:44:12 +0100

    day 6 (cleaned up)

--- 2020/day_06.py
+++ 2020/day_06.py
@@ -1,6 +1,6 @@
 with open('./data/day_06.txt') as fd:
-    test_data = fd.read().split('\n\n')
+    data = fd.read().split('\n\n')
 
-print(sum([len(set(x.replace('\n', ''))) for x in test_data]))
+print(sum([len(set(x.replace('\n', ''))) for x in data]))
 # looks a bit creepy, I admit :(
-print(sum([len(set.intersection(*map(set, x.split('\n')))) for x in test_data]))
+print(sum([len(set.intersection(*map(set, x.split('\n')))) for x in data]))
